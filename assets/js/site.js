b0c50f062acd8ebd0f062cadfefe3dc53b71ca59
sudipto <sudiptocsi@gmail.com>
2021-11-02 14:17:16 +0600

    complete the calculator form summation formula

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,34 +1,66 @@
-$(function(){
-    //var all = document.querySelectorAll(".bdt-ep-field-wrap input, bdt-ep-field-wrap select");
-    //var all = document.querySelectorAll(".bdt-ep-field-wrap input, .bdt-ep-field-wrap select");
-    var all = $(".bdt-ep-field-wrap");
-    $.each($(".bdt-ep-field-wrap"), function( index, value ) {
-        if($(this).find("input[type=text]")){
-            console.log($(this, 'input[type=text]').val());
-        }
-    });
-    return;
-    console.log(all);
-    return;
-    var data = [];
-    for (var i=0; i < all.length; i++) {
-        //console.log("type : " + all[i].value);
-        //console.log($(all[i]).val());
-        console.log(all[i].tagName);
-        if(all[i].tagName === 'INPUT'){
+function calculate_result(){
+    let data = [], variableIndex, formulaString = '', result = 0;
+    $.each($(".bdt-ep-field-wrap input[type=text], .bdt-ep-field-wrap input[type=hidden], .bdt-ep-field-wrap input[type=checkbox], .bdt-ep-field-wrap input[type=radio], .bdt-ep-field-wrap input[type=number]"),
+      function (index, item) {
+        variableIndex = parseInt(index) + 1;
+        let itemValue = parseInt($(item).val());
+        if ($(item).prop("type") === "checkbox" || $(item).prop("type") === "radio") {
+          if ($(item).is(":checked") === true) {
             data.push({
-                type: 'INPUT',
-                index: i,
-                value: all[i].value
+              type: $(item).prop("type"),
+              index: index,
+              //value: itemValue,
+              value: $(item).val(),
+              variable: "f" + variableIndex
             });
-        } 
-        if(all[i].tagName === 'SELECT'){
+            formulaString += Number.isInteger(itemValue) && itemValue < 0 ? "-f" + variableIndex + ', ' : "f" + variableIndex + ', ';
+            variableIndex++;
+          }
+        } else if ($(item).prop("type") === "number") {
             data.push({
-                type: 'SELECT',
-                index: i,
-                value: all[i].value
+              type: $(item).prop("type"),
+              index: index,
+              value: $(item).val(),
+              variable: "f" + variableIndex
             });
+            //formulaString += "f" + variableIndex + ', ';
+            formulaString += Number.isInteger(itemValue) && itemValue < 0 ? "-f" + variableIndex + ', ' : "f" + variableIndex + ', ';
+            variableIndex++;
+          //}
+        } else {
+          data.push({
+            type: $(item).prop("type"),
+            index: index,
+            value: $(item).val(),
+            variable: "f" + index + 1
+          });
+          //formulaString += "f" + variableIndex + ', ';
+          formulaString += Number.isInteger(itemValue) && itemValue < 0 ? "-f" + variableIndex + ', ' : "f" + variableIndex + ', ';
+          variableIndex++;
         }
+      }
+    );
+    /**
+     * its means that sum event have to trigger here
+     */
+    if(data.length > 0){
+      for(let i = 0; i < data.length; i++)
+      {
+          let getValue = parseInt(data[i].value);
+          if(Number.isInteger(getValue)){
+            result += getValue;
+          }
+      }
+      $(".bdt-result-count p span").text(result);
+      $(".uk-textarea").text("SUM(" + formulaString.replace(/,\s*$/, "") + ")"); // removing comma at end
     }
-    console.log(data);
+}
+/**
+ * trigger on ready default
+ */
+$(function(){
+    calculate_result();
+    $(".bdt-ep-calculator-form").change(function(){
+        calculate_result();
+    });
 });
